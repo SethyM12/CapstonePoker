fcff8a3e4461f6272f7ee831a721c64097a69347
SethyM12 <seth50821@outlook.com>
2026-04-06 08:59:22 -0400

    Getting a nice UI

--- MilesHighPoker/MilesHighPoker/Components/Pages/Home.razor.cs
+++ MilesHighPoker/MilesHighPoker/Components/Pages/Home.razor.cs
@@ -1,20 +1,9 @@
+using MilesHighPoker.GameLogic;
+
 namespace MilesHighPoker.Components.Pages;
 
 public partial class Home
 {
-    private readonly string?[] CommunityCards = new string?[5];
-
-    private void SetCommunityCard(int slot, string card)
-    {
-        if (slot < 0 || slot >= CommunityCards.Length) return;
-        CommunityCards[slot] = card;
-    }
-
-    private void ClearCommunityCards()
-    {
-        for (var i = 0; i < CommunityCards.Length; i++)
-        {
-            CommunityCards[i] = null;
-        }
-    }
+    private Card?[] CommunityCards { get; set; } = new Card[5];
+    private uint Pot { get; set; } = 0;
 }
